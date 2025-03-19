d8c1fe0360cacacdaf6a24c47a089b88aa5a7625
ThysB <baamy004@mymail.unisa.edu.au>
2025-03-19 15:19:26 +1100

    - implement transaction.py and test_transaction.py - set-up pytest.ini with path

--- models/transaction.py
+++ models/transaction.py
@@ -6,3 +6,24 @@ class Transaction:
         self.transaction_date = transaction_date
         self.description = description
         self.amount = amount
+        self._label = ""
+
+    def get_id(self):
+        return self.transaction_id
+
+    def get_date(self):
+        return self.transaction_date
+
+    def get_description(self):
+        return self.description
+
+    def get_amount(self):
+        return self.amount
+
+    def set_label(self, label):
+        self._label = label
+
+    def get_label(self):
+        return self._label
+
+
--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -1,5 +1,32 @@
 from datetime import date
 from models.transaction import Transaction
 
+
 def test_transaction_creation():
-    pass
+    # Arrange
+    transaction_id = 1
+    transaction_date = date(2025, 3, 18)
+    description = "Soy Latte"
+    amount = 5.50
+
+    # Act
+    transaction = Transaction(transaction_id, transaction_date, description, amount)
+
+    # Assert
+    assert transaction.get_id() == transaction_id
+    assert transaction.get_date() == transaction_date
+    assert transaction.get_description() == description
+    assert transaction.get_amount() == amount
+    assert transaction.get_label() == ""
+
+def test_transaction_label():
+    # Arrange
+    transaction = Transaction(1, date(2025, 3, 18), "Large Latte", 6.50)
+
+    # Act
+    transaction.set_label("Food")
+
+    # Assert
+    assert transaction.get_label() == "Food"
+
+
