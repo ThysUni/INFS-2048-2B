24dd23a99bc5b2d5c08dfe3bd9501b17b0e4223c
ThysB <baamy004@mymail.unisa.edu.au>
2025-03-22 20:17:31 +1100

    - change loading rules this means testing works from main folder (not from tests folder)

--- tests/test_csv_rule_access.py
+++ tests/test_csv_rule_access.py
@@ -6,7 +6,7 @@ def test_load_rules():
     rule_access = CSVRuleAccess()
 
     # Act
-    pattern_count = rule_access.load_rules("../examples/patterns.csv")
+    pattern_count = rule_access.load_rules("examples/patterns.csv")
 
     # Assert
     assert pattern_count == 20  # 20 individual patterns
@@ -17,7 +17,7 @@ def test_get_rule():
     rule_access = CSVRuleAccess()
 
     # Act
-    rule_access.load_rules("../examples/patterns.csv")
+    rule_access.load_rules("examples/patterns.csv")
 
     # Assert
     assert rule_access.get_rule("coffee") == "Food"
@@ -32,3 +32,6 @@ def test_get_rule():
     # Test empty string
     assert rule_access.get_rule("") is None
 
+#TODO
+# only works when run in main folder
+# if possible change it so it works from both
